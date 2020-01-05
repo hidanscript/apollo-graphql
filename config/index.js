c2095554af37736565f46107d4d6f5a9f0ea0263
hidanscript <hidanshira2@gmail.com>
2020-01-05 04:14:45 -0300

    First Commit

---
+++ config/index.js
@@ -0,0 +1,6 @@
+// Configuration
+import config from './config.json';
+
+export const $db = () => config.db;
+export const $security = () => config.security;
+export const $serverPort = () => config.serverPort;
---
+++ src/models/index.js
@@ -0,0 +1,30 @@
+import Sequelize from 'sequelize';
+
+// Configuration
+
+import { $db } from '../../config';
+
+const { database, username, password, dialect } = $db();
+
+const sequelize = new Sequelize(database, username, password, {
+    dialect,
+    define: {
+        uderscored: true
+    }
+});
+
+const models = {
+    Post: sequelize.import("./Post"),
+    Tag: sequelize.import("./Tag"),
+    User: sequelize.import("./User"),
+}
+
+Object.keys(models).forEach(modelName => {
+    if('associate' in models[modelName]) {
+        models[modelName].associate(models);
+    }
+});
+
+models.sequelize = sequelize;
+
+export default models;
